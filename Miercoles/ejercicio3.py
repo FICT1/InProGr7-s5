8b97d5670aa158388be43e4c526b69cd3ff44bd4
FICT1 <ficallejas@uamv.edu.ni>
2025-04-23 13:33:34 -0600

    ejercicio 3

---
+++ Miercoles/ejercicio3.py
@@ -0,0 +1,11 @@
+#notificacion de velocidad peligrosa en carretera
+#ingresar velocidad
+#si supera 120km/h alertar "REDUZCA LA VELOCIDAD"
+
+velocidad=int(input("A que velocidad vas? "))
+if (velocidad > 120):
+    print("REDUZCA LA VELOCIDAD")
+else:
+    print("Velocidad dentro de los limites permitidos")
+#fin del programa
+
